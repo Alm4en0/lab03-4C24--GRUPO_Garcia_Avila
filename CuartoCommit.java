6a5e1e9811ecacba1397325e96bf1e8d0c5ba356
Alm4en0 <almendra.garcia@tecsup.edu.pe>
2024-04-03 13:23:08 -0500

    Agregando el tercer archivo

---
+++ CuartoCommit.java
@@ -0,0 +1,12 @@
+public class EjemploBranch {
+    public static void main(String[] args) {
+        System.out.println("¡Este es el archivo de ejemplo!");
+        
+        // Agregamos una nueva función en el branch
+        saludar("Rebeca");
+    }
+    
+    public static void saludar(String nombre) {
+        System.out.println("Hola, " + nombre + "Acabo de crear el cuarto archivo de ejemplo ");
+    }
+}

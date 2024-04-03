b3fd69ad07fa373abd4b354ab0367a80ace10219
Alm4en0 <almendra.garcia@tecsup.edu.pe>
2024-04-03 13:20:29 -0500

    Agregando el segundo archivo

---
+++ SegundoCommit.java
@@ -0,0 +1,12 @@
+public class EjemploBranch {
+    public static void main(String[] args) {
+        System.out.println("¡Este es el archivo dd ejemplo!");
+        
+        // Agregamos una nueva función en el branch
+        saludar("Rebeca");
+    }
+    
+    public static void saludar(String nombre) {
+        System.out.println("Hola, " + nombre + "Acabo de crear un archivo de ejemplo ");
+    }
+}
